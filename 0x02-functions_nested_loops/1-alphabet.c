f5a07e5586c6a5474024d053e44c77cde8464e10
code-mar21 <i.marusisay@gmail.com>
2022-09-13 22:27:27 +0300

    this commit belongs to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - show alphabets in small letter
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
