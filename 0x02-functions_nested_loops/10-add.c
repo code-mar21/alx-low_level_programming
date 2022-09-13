0cba60ce72d8f485b318ce385c01a23559ca52f8
code-mar21 <i.marusisay@gmail.com>
2022-09-13 23:47:44 +0300

    print num upto 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - returns the sum two numbers
+ * @n: is an int
+ * @e: is an int
+ * Return: The result of the addition.
+*/
+
+int add(int n, int e)
+{
+	return (n + e);
+}
